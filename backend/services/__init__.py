891cd04a867c32f329f5c359b41dc91e8aa581b0
[Aaditya1273] <aadityaiitian1515@gmail.com>
2026-02-14 18:05:20 +0530

    changes

--- backend/services/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-FarmOracle Services Package
-"""
--- frontend/src/test-build.js
+++
@@ -1,5 +0,0 @@
-// Simple test file to verify build works
-console.log("FarmOracle build test - v1.0.1");
-export default function TestBuild() {
-  return null;
-}
